fab02d720063ecc2a7cc75742ce96be768869137
AnkushGupta6 <ankushgupta@gmail.com>
2020-11-24 13:34:30 +0530

    weather final

--- app/src/main/java/com/example/farming/MainActivity.java
+++ app/src/main/java/com/example/farming/MainActivity.java
@@ -20,9 +20,31 @@ public class MainActivity extends AppCompatActivity {
         weatherImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //weather Activity
                 Intent weatherIntent = new Intent(MainActivity.this, WeatherActivity.class);
                 startActivity(weatherIntent);
             }
         });
+
+        ImageView newsImg = findViewById(R.id.newsImg);
+        newsImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //news Activity
+                Intent newsIntent = new Intent(MainActivity.this, ProgressActivity.class);
+                startActivity(newsIntent);
+            }
+        });
+
+        ImageView contactImg = findViewById(R.id.contactImg);
+        contactImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //contact Activity
+                Intent newsIntent = new Intent(MainActivity.this, ProgressActivity.class);
+                startActivity(newsIntent);
+            }
+        });
+
     }
 }
